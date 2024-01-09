6d5b1eca4c07c40cde8cdc314df056c864bd02bc
Varaygue <38994398+Varaygue@users.noreply.github.com>
2024-01-09 16:22:28 +0100

    Modifications Blockout
    Modifications Blockout

---
+++ Assets/Directory/Scripts/SC_SceneSwap.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SC_SceneSwap : MonoBehaviour
+{
+    public string sceneToSwap;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(sceneToSwap);
+    }
+}
