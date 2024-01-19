7feffb1536dae96a31dc8c8a9ec1090f9e707f08
Angelo "Kmadoh <a_lamouroux@stu-lisaa.com>
2024-01-19 16:48:10 +0100

    Menu end

--- Assets/Directory/Scripts/SC_LoadScene.cs
+++ Assets/Directory/Scripts/SC_LoadScene.cs
@@ -6,8 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class SC_LoadScene : MonoBehaviour
 {
+    public GameObject creditPanel;
     public void LoadScene(string Scene_Name)
     {
         SceneManager.LoadScene(Scene_Name);
     }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void CreditsButton()
+    {
+        creditPanel.SetActive(true);
+    }
+    public void LeaveCredits()
+    {
+        creditPanel.SetActive(false);
+    }
+
 }
