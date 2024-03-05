14d456a70f78ba9cfe4ee7a0e147f951d59646dd
Varaygue <38994398+Varaygue@users.noreply.github.com>
2024-03-05 17:46:24 +0100

    A
    A

--- Assets/Directory/Scripts/SC_PlayerDeath.cs
+++ Assets/Directory/Scripts/SC_PlayerDeath.cs
@@ -9,6 +9,7 @@ public class SC_PlayerDeath : MonoBehaviour
     public SC_GameManager GMscript;
     public GameObject deathScreen;
     public FirstPersonController playerScript;
+    public Transform lavaPos;
     private void OnTriggerEnter(Collider Player)
 {
     if (Player.gameObject.CompareTag("Darts"))
@@ -36,6 +37,7 @@ private void DeathScreen()
 
 public void RetryButton()
 {
+    lavaPos.position = new Vector3(-82.75f,-74.3f,0f);
     transform.position = GMscript.lastCheckPointPos;
     Cursor.visible=false;
     Cursor.lockState=CursorLockMode.Locked;
